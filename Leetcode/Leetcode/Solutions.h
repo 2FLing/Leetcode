c3e1d764dfd1dfd04ae0bb6c0b97675951acc90f
2FLing <349332929yaofu@gmail.com>
2019-11-24 15:52:32 -0800

    upload leetcode reverse

--- Leetcode/Leetcode/Solutions.h
+++ Leetcode/Leetcode/Solutions.h
@@ -2,6 +2,7 @@
 #ifndef _SOLUTIONS
 #define _SOLUTIONS
 #include<vector>
+#include<iostream>
 using namespace std;
 class Solutions
 {
@@ -21,6 +22,20 @@ public:
 			index++;
 		}
 		return element;
+	}
+	int reverse(int x)
+	{
+		int one, two, three,result;
+		one = x / 100;
+		two = x / 10 % 10;
+		three = x % 10;
+		if (three != 0)
+		{
+			result = three * 100 + two * 10 + one;
+		}
+		else
+			result = two * 10 + one;
+		return result;
 	}
 };
 #endif
--- Leetcode/Leetcode/Source.cpp
+++ Leetcode/Leetcode/Source.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 int main()
 {
-	vector<int>vec1,vec2;
+	vector<int>vec1, vec2;
 	vector<int>::iterator it;
 	Solutions s1;
+	int num;
 	vec1.push_back(2);
 	vec1.push_back(7);
 	vec1.push_back(11);
 	vec1.push_back(15);
-	vec2=s1.two_sums(vec1, 9);
+	vec2 = s1.two_sums(vec1, 9);
 	for (it = vec2.begin(); it != vec2.end(); it++)
 		cout << *it << " ";
 	cout << endl;
+	num = s1.reverse(120);
+	cout << num << endl;
 	return 0;
 }
